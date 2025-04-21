125febed2889c97f213f3998b0dd4c8438167013
sb1994 <sean94@gmail.com>
2025-04-22 00:23:19 +0100

    BL-12-create-the-the-job-applications-model Added the application model

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const propertyRoutesV1 = require("./v1/routes/propertyRoutes");
 const departmentRoutesV1 = require("./v1/routes/departmentRoutes");
 const jobRoleRoutesV1 = require("./v1/routes/jobRoleRoutes");
 const jobPostingRoutesV1 = require("./v1/routes/jobPostingRoutes");
+const jobApplicationRoutesV1 = require("./v1/routes/jobApplicationRoutes");
 
 const { default: mongoose } = require("mongoose");
 
@@ -48,6 +49,7 @@ app.use("/api/v1/property", propertyRoutesV1);
 app.use("/api/v1/departments", departmentRoutesV1);
 app.use("/api/v1/jobroles", jobRoleRoutesV1);
 app.use("/api/v1/jobpostings", jobPostingRoutesV1);
+app.use("/api/v1/jobapplications", jobApplicationRoutesV1);
 
 // Connection
 const PORT = process.env.PORT || 8080;
